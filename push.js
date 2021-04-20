64034388644bef4bbe7c586f771b9360d154825a
Shafayet Hossain <80921337+shafayet1372@users.noreply.github.com>
2021-04-21 03:05:48 +0600

    Add files via upload

---
+++ push.js
@@ -0,0 +1,11 @@
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+
+function myPush(arr,value){
+
+arr[arr.length]=value
+
+}
+myPush(fruits,"sdsds")
+myPush(fruits,"dfdf")
+console.log(fruits)
